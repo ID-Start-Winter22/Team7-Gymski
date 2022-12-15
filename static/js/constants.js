1693be894fdb75fb7b2322687433c5815664f4ab
Oliver Schlieper <oliver.schlieper@icloud.com>
2022-12-15 19:06:17 +0100

    plswork

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "18.192.103.132";
+const rasa_server_url = "http://18.192.103.132/webhooks/rest/webhook";
 const sender_id = uuidv4();
