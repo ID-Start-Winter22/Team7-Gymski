a53bff09ff296c7fcffd1d7dc100e81e84065d1d
Oliver Schlieper <oliver.schlieper@icloud.com>
2022-12-15 18:52:05 +0100

    aws constant

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "http://localhost:5005/webhooks/rest/webhook";
+const rasa_server_url = "18.192.103.132";
 const sender_id = uuidv4();
