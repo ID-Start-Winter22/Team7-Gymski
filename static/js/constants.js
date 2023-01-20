6c3d90458756a3c8f93a5cd341915769cd4c193c
TeschNils <nils.tesch03@gmail.com>
2023-01-20 11:00:21 +0100

    changes

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,4 @@
 const action_name = "action_hello_world";
 const rasa_server_url = "http://localhost:5005/webhooks/rest/webhook";
+// const rasa_server_url = "http://34.244.29.62/webhooks/rest/webhook";
 const sender_id = uuidv4();
